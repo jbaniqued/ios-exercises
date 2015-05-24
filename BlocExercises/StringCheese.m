18c6672a21a3cd147d37701e95d77713e5a6b024
Joanne Baniqued <jojodivine@gmail.com>
2015-05-24 16:28:22 -0400

    merged my first branch

---
+++ BlocExercises/StringCheese.m
@@ -0,0 +1,37 @@
+//
+//  StringCheese.m
+//  BlocExercises
+//
+//  Created by Aaron on 6/9/14.
+//
+//
+
+#import "StringCheese.h"
+
+@implementation StringCheese
+
+- (NSString *) favoriteCheeseStringWithCheese:(NSString *)cheeseName {
+    /* WORK HERE */
+    return nil;
+}
+
+- (NSString *) cheeseNameWithoutCheeseSuffix:(NSString *)cheeseName {
+    /* WORK HERE */
+    return nil;
+}
+
+- (NSString *) numberOfCheesesStringWithCheeseCount:(NSUInteger)cheeseCount {
+    if (cheeseCount == 1) {
+        /* WORK HERE, ASSUMING THERE IS 1 CHEESE */
+    } else {
+        /* WORK HERE, ASSUMING THERE ARE 2+ CHEESES */
+    }
+    
+    /*
+     (You will learn more about if/else statements in the next checkpoint.)
+     */
+    
+    return nil;
+}
+
+@end
