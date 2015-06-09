2b902913f08daa60b3b0812a63123653c2e9b288
Joanne Baniqued <jojodivine@gmail.com>
2015-06-09 15:58:16 -0400

    Completed if/else statement

--- BlocExercises/MarysAppleHandler.m
+++ BlocExercises/MarysAppleHandler.m
@@ -14,21 +14,26 @@
     NSString *itemToReturn;
 
     /* WORK HERE */
+    if (dollars >= 100000000) {
+        itemToReturn = @"have The Big Apple";
+    } else if (dollars >= 1000) {
+        itemToReturn = @"have an Apple computer";
+    } else if (dollars >= 6) {
+        itemToReturn = @"have an apple";
+    } else if (dollars >= 5) {
+        itemToReturn = @"have some gum";
+    } else {
+        itemToReturn = @"get out of my store";
+    }
     
-    NSLog(@"For $%ld, Mary can: %@", (long)dollars, itemToReturn);
+    NSLog(@"For $%d, Mary can: %@", dollars, itemToReturn);
     return itemToReturn;
 }
 
 - (NSUInteger) dollarCostForAppleFlavoredVodka {
     /* WORK HERE */
+    return self.getsDiscount ? 18 : 24;
 
-    NSUInteger cost = 24;
-    
-    if (self.getsDiscount) {
-        cost *= .75;
-    }
-    
-    return cost;
 }
 
 @end
