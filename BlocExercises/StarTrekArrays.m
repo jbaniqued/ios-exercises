40406a527332eeb784c25916b938fd616a49c66e
Joanne Baniqued <jojodivine@gmail.com>
2015-06-11 22:40:54 -0400

    Need assistance with testing Arrays

--- BlocExercises/StarTrekArrays.m
+++ BlocExercises/StarTrekArrays.m
@@ -12,22 +12,34 @@
 
 - (NSArray *) arrayOfStarTrekCharactersFromString:(NSString *)characterString {
     /* WORK HERE */
-    return @[];
+    return [characterString componentsSeparatedByString:@";"];
 }
+
+    //got this one
 
 - (NSString *) stringOfStarTrekCharactersFromArray:(NSArray *)characterArray {
     /* WORK HERE */
-    return @"";
+    return [characterArray componentsJoinedByString:@";"];
 }
+
+    //got this one.
 
 - (NSArray *) alphabeticallySortedStarTrekCharactersFromArray:(NSArray *)characterArray {
     /* WORK HERE */
-    return @[];
+    NSArray *startrekArray = [characterArray sortedArrayUsingSelector:@selector(caseInsensitiveCompare:)];
+    return startrekArray;
 }
+    //I don't get why I am using sortedArrayUsingSelector and why characterArray sortUsingDescriptors: wouldn't work for me...
+
 
 - (BOOL) characterArrayContainsWorf:(NSArray *)characterArray {
     /* WORK HERE */
-    return NO;
-}
-
+    NSPredicate *worfIsPresent = [NSPredicate predicateWithFormat:@"SELF CONTAINS[c] 'Worf'"];
+    for (NSString *characterName in characterArray) {
+        NSString *lowercaseName = characterName.lowercaseString;
+        NSRange worfRange = [lowercaseName rangeOfString:@"worf"];
+    }
+    }
 @end
+
+    //Completely lost on NSPredicate....
